b4136aaf67369fae4a0d1d507cb3df25b6eeb37f
Daniel de Koning <danieldekoning66@gmail.com>
2026-07-01 18:01:55 +0200

    auto: update popup.js

--- extension/popup.js
+++ extension/popup.js
@@ -1,4 +1,4 @@
-const SERVER_URL = "https://api.crosslisteu.com";
+const SERVER_URL = "https://crosslisteu.com";
 
 async function checkLoginState() {
   const { authToken, userEmail } = await chrome.storage.sync.get(["authToken", "userEmail"]);
